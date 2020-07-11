29ba84ee5472441803e6ab984f0774aa5aae9b88
Yoon Sean Chen <chen.ys@nettium.net>
2020-07-11 18:33:41 +0800

    Enable MVC middle ware, include and route Sample controller

---
+++ FactoryMethod.API/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FactoryMethod.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+        //[HttpGet("api/sample")]
+        public JsonResult GetSamples()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "Sample 1" },
+                    new { id = 2, Name = "Sample 2" }
+                });
+        }
+    }
+}
